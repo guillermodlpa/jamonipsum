f3b90195ecb22f57229cba1593162cf5f2381358
Guillermo de la Puente <g.puente.allott@gmail.com>
2016-12-18 22:06:55 +0100

    Better algorithm, styles, reload button, words

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 (function() {
-  var initialTokens = ['Jamon', ' ', 'ipsum', ' '];
-
   function getCount() {
     return document.getElementById('jamon-count-input').value;
   }
@@ -9,6 +7,10 @@
   }
   function getResultHTMLNode() {
     return document.getElementById('jamon-result');
+  }
+  function bindGenerateButton() {
+    var button = document.getElementById('jamon-button');
+    button && button.addEventListener('click', jamonIpsum);
   }
 
   function jamonIpsum() {
@@ -19,45 +21,119 @@
   }
 
   function generateTokens() {
-    var tokens = initialTokens.slice();
+    var tokens = initialWords.slice();
     var count = getCount();
 
-    var nonConnectorWords = 0;
-    var minimumRealWords = 3;
-    var chancesOfConnector = .1;
     var lastTokens = [];
+    var wordsAdded = tokens.length;
+    var newWord;
 
-    while (tokens.length < count) {
+    var nonConnectors = 0;
+    var nonArticles = 0;
+    var nonStops = 0;
+
+    while (wordsAdded < count) {
       lastTokens = tokens.slice(-10);
 
-      tokens.push(getRandomWord(lastTokens));
+      newWord = getRandomWord(lastTokens);
 
-      // Use connector or whitespace?
-      if (
-        nonConnectorWords >= minimumRealWords &&
-        Math.random() > chancesOfConnector
-      ) {
-        nonConnectorWords = 0;
-        tokens.push(getRandomConnector(), ' ')
-      } else {
-        nonConnectorWords++;
-        tokens.push(' ');
+      // If last char is a `.`, uppercase.
+      if (tokens[tokens.length - 1].slice(-2) === '. ') {
+        newWord = newWord.charAt(0).toUpperCase() + newWord.substr(1);
       }
+
+      wordsAdded++;
+
+      var extraTokenType = getNextTokenType(count - wordsAdded, nonConnectors, nonArticles, nonStops);
+      var extraToken;
+
+      switch (extraTokenType) {
+        case 'connector':
+          extraToken = getRandomConnector() + ' ';
+          nonConnectors = 0;
+          nonArticles++;
+          wordsAdded++;
+          nonStops++;
+          break;
+        case 'article':
+          extraToken = getRandomArticle() + ' ';
+          nonArticles = 0;
+          nonConnectors++;
+          wordsAdded++;
+          nonStops++;
+          break;
+        case 'stop':
+          extraToken = getRandomStop() + ' ';
+          nonConnectors++;
+          nonArticles++;
+          nonStops = 0;
+          break;
+        default:
+          extraToken = ' ';
+          nonConnectors++;
+          nonArticles++;
+          nonStops++;
+      }
+
+      tokens.push(newWord + extraToken);
     }
 
-    tokens.pop(); // last whitespace.
+    // Remove last whitespace.
+    tokens[tokens.length - 1] = tokens[tokens.length - 1].slice(0, -1);
     tokens.push('.');
 
     return tokens;
+  }
+
+  var minWordsBetweenStops = 3;
+  var chancesOfStop = .2;
+  var minWordsBetweenConnectors = 6;
+  var chancesOfConnector = .2;
+  var minWordsBetweenArticles = 2;
+  var chancesOfArticle = .5;
+
+  function getNextTokenType(wordsLeft, nonConnectors, nonArticles, nonStops) {
+    // Article.
+    if (
+      wordsLeft > 2 &&
+      nonArticles >= minWordsBetweenArticles &&
+      Math.random() > chancesOfArticle
+    ) {
+      return 'article';
+    // Connector.
+    } else if (
+      wordsLeft > 6 &&
+      nonConnectors >= minWordsBetweenConnectors &&
+      Math.random() > chancesOfConnector
+    ) {
+      return 'connector';
+    // Stop.
+    } else if (
+      wordsLeft > 6 &&
+      nonStops >= minWordsBetweenStops &&
+      Math.random() > chancesOfStop
+    ) {
+      return 'stop';
+    } else {
+      return undefined; // whitespace.
+    }
   }
 
   function getRandInt(limit) {
     return Math.floor(Math.random() * limit);
   }
 
+  function getRandomStop() {
+    var rand = getRandInt(availableStops.length);
+    return availableStops[rand];
+  }
   function getRandomConnector() {
     var rand = getRandInt(availableConnectors.length);
     return availableConnectors[rand];
+  }
+  function getRandomArticle() {
+    var rand = getRandInt(availableArticles.length);
+    return availableArticles[rand];
   }
 
   function getRandomWord(lastTokens) {
@@ -74,13 +150,31 @@
     return word;
   }
 
-  var availableConnectors = [
+  var availableStops = [
     '.',
     ',',
+  ];
+
+  var availableConnectors = [
     ' y',
     ' pero',
   ];
 
+  var availableArticles = [
+    ' la',
+    ' el',
+    ' las',
+    ' los',
+    ' mis',
+    ' tus',
+    ' sus',
+    ' tu',
+    ' mi',
+    ' un',
+    ' una',
+  ];
+
+  var initialWords = ['Jamón ', 'ipsum '];
   var availableWords = [
     'jamón',
     'estopa',
@@ -91,11 +185,6 @@
     'salmorejo',
     'canción',
     'señor',
-    'el',
-    'la',
-    'los',
-    'las',
-    'con',
     'amiga',
     'estupendo',
     'croquetas',
@@ -110,7 +199,41 @@
     'charanga',
     'chiringuito',
     'playita',
+    'persiana',
+    'bocadillo',
+    'fregona',
+    'tomatito',
+    'colacao',
+    'café con leche',
+    'turrón',
+    'pipas',
+    'tío',
+    'tía',
+    'botellón',
+    'chollazo',
+    'flipado',
+    'mola mazo',
+    'cocido',
+    'al tun tun',
+    'fútbol',
+    'más fútbol',
+    'quinto pino',
+    'piripi',
+    'ligar',
+    'vale',
+    'canturreando',
+    'vagueando',
+    'haciéndose el loco',
+    'pega ojo',
+    'pan comido',
+    'tócate',
+    'enchufe',
+    'flamenco',
+    'Almodóvar',
+    'Penélope Cruz',
+    'Rey',
   ];
 
+  bindGenerateButton();
   jamonIpsum();
 }());
